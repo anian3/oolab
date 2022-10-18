422625f03da3ce186efd556827c3f81f454ca1be
Ania Nowacka <anna.nowacka146@gmail.com>
2022-10-18 20:57:23 +0200

    lab01

---
+++ src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,35 @@
+package agh.ics.oop;
+
+public class World {
+
+    public static void main(String[] args) {
+        System.out.println("Start");
+        run(conversion(args));
+        System.out.println("Stop");
+    }
+
+    public static void run(Direction[] dir) {
+        for (Direction argument : dir) {
+            String message = switch (argument) {
+                case FORWARD -> "Zwierzak idzie do przodu.";
+                case BACKWARD -> "Zwierzak idzie do tyłu.";
+                case LEFT -> "Zwierzak skręca w lewo.";
+                case RIGHT -> "Zwierzak skręca w prawo.";
+            };
+            System.out.println(message);
+        }
+    }
+    public static Direction[] conversion(String[] args){
+        Direction[] dir = new Direction[args.length];
+        for (int i=0; i<args.length; ++i) {
+            dir[i] = switch (args[i]) {
+                case "f" -> Direction.FORWARD;
+                case "b" -> Direction.BACKWARD;
+                case "l" -> Direction.LEFT;
+                case "r" -> Direction.RIGHT;
+                default -> null;
+            };
+        }
+        return dir;
+    }
+}
